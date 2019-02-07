53b4111eb266c08adf08054c0376103b645f8cd3
Felipe Bonon Goncalves <fbonon@ciandt.com>
2019-02-07 12:43:50 -0200

    Resolução Exercício 1

--- src/main/java/br/com/cit/dojo/ex1/InsuranceService.java
+++ src/main/java/br/com/cit/dojo/ex1/InsuranceService.java
@@ -4,8 +4,30 @@ public class InsuranceService {
 
     public static double calculateInvoiceValue(int age, boolean driver, boolean healthy) {
 
-        // Do stuff here...
+        double value = 0;
 
-        return 0;
+        if (age < 18) {
+            if (driver) {
+                throw new IllegalArgumentException("Não é permitido um motorista ter menos de 18 anos");
+            }
+
+            value = 350;
+        } else if (age <= 30 && driver) {
+            value = 600;
+        } else if (age < 50) {
+            value = 500;
+        } else {
+            value = 900;
+        }
+
+        if (!healthy) {
+            value += 200;
+
+            if (age >= 50) {
+                value *= 0.9;
+            }
+        }
+
+        return value;
     }
 }
