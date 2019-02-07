50e78d66ffd3a274947f5dc7eea71efd9e81a4ba
Felipe Bonon Goncalves <fbonon@ciandt.com>
2019-02-07 15:20:19 -0200

    Resolução Ex4

--- src/main/java/br/com/cit/dojo/ex4/CashMachineService.java
+++ src/main/java/br/com/cit/dojo/ex4/CashMachineService.java
@@ -5,10 +5,22 @@ import java.util.List;
 
 public class CashMachineService {
 
+    public static final int[] MACHINE_NOTES = new int[]{50, 10, 5, 1};
+
     public static List<Integer> getNotes(int value) {
+        List<Integer> cashOutNotes = new ArrayList<Integer>();
 
-        // Do stuff here...
+        if (value >= 0) {
+            int remainingValue = value;
 
-        return new ArrayList<Integer>();
+            for (int bankNote : MACHINE_NOTES) {
+                cashOutNotes.add(remainingValue / bankNote);
+                remainingValue %= bankNote;
+            }
+        } else {
+            throw new IllegalArgumentException("O valor a ser sacado não pode ser negativo");
+        }
+
+        return cashOutNotes;
     }
 }
