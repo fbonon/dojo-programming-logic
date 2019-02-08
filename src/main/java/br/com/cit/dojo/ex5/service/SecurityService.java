b2b33bdc7380bdaae859aa7c48b51b11cb4cc450
Felipe Bonon Goncalves <fbonon@ciandt.com>
2019-02-08 14:33:25 -0200

    Resolução Ex5

--- src/main/java/br/com/cit/dojo/ex5/service/SecurityService.java
+++ src/main/java/br/com/cit/dojo/ex5/service/SecurityService.java
@@ -1,13 +1,31 @@
 package br.com.cit.dojo.ex5.service;
 
 import br.com.cit.dojo.ex5.model.Customer;
+import br.com.cit.dojo.ex5.model.OrderHistory;
 
 public class SecurityService {
 
     public static boolean verifyCustomerCredibility(Customer customer) {
+        if (customer == null) {
+            return false;
+        }
 
-        // Do stuff here...
+        double totalOrderCount = 0;
+        double totalOrdersNotPaid = 0;
+        double totalOrdersDelayed = 0;
 
-        return false;
+        for (OrderHistory orderHistory : customer.getOrderHistoryList()) {
+            totalOrderCount++;
+
+            if (!orderHistory.isPaymentDone())
+                totalOrdersNotPaid++;
+
+            if (!orderHistory.isPaymentOnTime())
+                totalOrdersDelayed++;
+        }
+
+        return (totalOrdersNotPaid / totalOrderCount <= 0.3) &&
+                (totalOrdersDelayed / totalOrderCount <= 0.6) &&
+                ((customer.getBalance() > 0) || (customer.getBalance() == 0 && totalOrdersDelayed == 0));
     }
 }
