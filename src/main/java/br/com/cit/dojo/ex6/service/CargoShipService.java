b7519f59eaf7e099a9d64bf30a02e0eccbfec65c
Felipe Bonon Goncalves <fbonon@ciandt.com>
2019-02-18 12:30:48 -0300

    Resolucao Exercicio 6

--- src/main/java/br/com/cit/dojo/ex6/service/CargoShipService.java
+++ src/main/java/br/com/cit/dojo/ex6/service/CargoShipService.java
@@ -1,16 +1,21 @@
 package br.com.cit.dojo.ex6.service;
 
 import br.com.cit.dojo.ex6.model.Container;
+import org.apache.commons.collections4.ListUtils;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CargoShipService {
 
     public static List<Container> prepareContainersforShipping(List<Container> containers) {
+        List<Container> reefers = containers.stream().filter(Container::isReefer).collect(Collectors.toList());
+        List<Container> notReefers = containers.stream().filter(c -> !c.isReefer()).collect(Collectors.toList());
 
-        // Do stuff here...
+        Collections.sort(reefers, (Container c1, Container c2) -> c2.getWeight() - c1.getWeight());
+        Collections.sort(notReefers, (Container c1, Container c2) -> c2.getWeight() - c1.getWeight());
 
-        return new ArrayList<>();
+        return ListUtils.union(reefers, notReefers);
     }
 }
